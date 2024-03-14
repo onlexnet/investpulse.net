18679112dafff1523e261c0e6ee7c9e2605105b9
siudeks <slawomir@siudek.net>
2024-03-14 19:11:30 +0000

    Java packages

--- uservice-market/app.py
+++ uservice-market/app.py
@@ -5,6 +5,7 @@ import grpc
 
 from dapr.clients import DaprClient
 from market_rpc.onlexnet.market.events import MarketChangedEvent
+from avro import datafile, io
 
 APP_PORT=os.getenv('APP_PORT', 50052)
 log = logging.getLogger("myapp")
@@ -18,11 +19,11 @@ def serve():
     with DaprClient() as dc:
         event = MarketChangedEvent(date = 20010203)
         event_as_str = str(event)
+        event_as_bytes = event.to_avro_writable()
         logging.info("sparta")
-        logging.info(f"bytes: {event_as_str}")
-        print("spartaaa")
+        logging.info(f"Event as string: {event_as_str}")
         print(event_as_str.__class__)
-        resp = dc.publish_event(pubsub_name="pubsub", topic_name="TOPIC_A", data = event_as_str)
+        resp = dc.publish_event(pubsub_name="pubsub", topic_name="TOPIC_A", data = event_as_str, data_content_type="application/avro")
 
     server.wait_for_termination()
 
