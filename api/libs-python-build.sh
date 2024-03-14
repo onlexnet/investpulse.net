fd64c4284387ed67836b9c834333f1d1af1f953b
siudeks <slawomir@siudek.net>
2024-03-14 09:19:54 +0000

    Can emit json event

--- api/libs-python-build.sh
+++ api/libs-python-build.sh
@@ -7,8 +7,10 @@ cd python-3.11/app1_rpc
 python setup.py sdist
 popd
 
-
-python serialize_avro.py "./grpc/market_rpc/market.avsc" python-3.11/market_rpc/market_rpc
+# generate initial folder with __init__.py thanks to avro codegen
+rm -s python-3.11/market_rpc/dist/*
+python serialize_avro.py "./grpc/market_rpc/schema.avsc" python-3.11/market_rpc/market_rpc
+# ... and proto part as well
 OUT_DIR=./python-3.11/market_rpc
 python -m grpc_tools.protoc -I./grpc --python_out=$OUT_DIR --pyi_out=$OUT_DIR --grpc_python_out=$OUT_DIR ./grpc/market_rpc/*.proto
 pushd .
--- uservice-market/app.py
+++ uservice-market/app.py
@@ -3,8 +3,8 @@ import logging
 import os
 import grpc
 
-from market_rpc.market_pb2 import MarketChangedEvent
 from dapr.clients import DaprClient
+from market_rpc.onlexnet.market.events import MarketChangedEvent
 
 APP_PORT=os.getenv('APP_PORT', 50052)
 log = logging.getLogger("myapp")
@@ -16,18 +16,18 @@ def serve():
     server.start()
 
     with DaprClient() as dc:
-        event = MarketChangedEvent(date=20200102, open=1, close=2, low=3, high=4, adj_close=5)
-        event_as_bytes = event.SerializeToString()
-        logging.log("sparta")
-        logging.log(f"bytes: {event_as_bytes}")
+        event = MarketChangedEvent(date = 20010203)
+        event_as_str = str(event)
+        logging.info("sparta")
+        logging.info(f"bytes: {event_as_str}")
         print("spartaaa")
-        print(event_as_bytes.__class__)
-        resp = dc.publish_event(pubsub_name="pubsub", topic_name="TOPIC_A", data = event_as_bytes)
+        print(event_as_str.__class__)
+        resp = dc.publish_event(pubsub_name="pubsub", topic_name="TOPIC_A", data = event_as_str)
 
     server.wait_for_termination()
 
 if __name__ == '__main__':
-    logging.basicConfig()
+    logging.basicConfig(level=logging.DEBUG)
     log.setLevel(logging.INFO)
 
     log.info(f"port: {APP_PORT}")
