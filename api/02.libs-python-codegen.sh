a0e73821977cd6a8f423d963d819e5588b20a5e1
siudeks <slawomir@siudek.net>
2024-03-17 00:41:03 +0000

    WIP

--- api/02.libs-python-codegen.sh
+++ api/02.libs-python-codegen.sh
@@ -9,37 +9,3 @@ python ./utils/serialize_avro.py market_rpc
 
 python ./utils/serialize_grpc.py scheduler_rpc
 python ./utils/serialize_avro.py scheduler_rpc
-
-# _PYTHON_LIBS_DIR=./python
-# _SCHEMA_DIR=./schema
-# _PROJECT_DIR=app1_rpc
-
-# _CODEGEN_ROOT_DIR=$_PYTHON_LIBS_DIR/$_PROJECT_DIR/$_PROJECT_DIR
-# _PROJECT_SCHEMA_DIR=$_SCHEMA_DIR/$_PROJECT_DIR
-
-# mkdir -p $_CODEGEN_ROOT_DIR
-# touch $_CODEGEN_ROOT_DIR/__init__.py
-
-# Generate ABRO
-# generate initial output folder, it will contains __init__.py thanks to avro codegen
-# rm -s python-3.11/market_rpc/dist/*
-# python serialize_avro.py "$_PROJECT_SCHEMA_DIR/schema.avsc" $_CODEGEN_ROOT_DIR
-
-# Generate GRPC
-# python -m grpc_tools.protoc --proto_path=$_PROJECT_SCHEMA_DIR --python_out=$_CODEGEN_ROOT_DIR --pyi_out=$_CODEGEN_ROOT_DIR --grpc_python_out=$_CODEGEN_ROOT_DIR $_PROJECT_SCHEMA_DIR/*.proto
-# pushd .
-# cd python-3.11/app1_rpc
-# python setup.py sdist
-# popd
-
-# # generate initial folder with __init__.py thanks to avro codegen
-# rm -s python-3.11/market_rpc/dist/*
-# python serialize_avro.py "./grpc/market_rpc/schema.avsc" python-3.11/market_rpc/market_rpc
-# # ... and proto part as well
-# OUT_DIR=./python-3.11/market_rpc
-# python -m grpc_tools.protoc -I./grpc --python_out=$OUT_DIR --pyi_out=$OUT_DIR --grpc_python_out=$OUT_DIR ./grpc/market_rpc/*.proto
-# pushd .
-# cd python-3.11/market_rpc
-# python setup.py sdist
-# popd
-
--- uservice-market/app.py
+++ uservice-market/app.py
@@ -10,36 +10,40 @@ from venv import logger
 import grpc
 import fastavro
 import json
+from datetime import date
+
+from pandas import DataFrame
+
+import src.YahooFinance as yf
 
 from dapr.clients import DaprClient
 from avro import datafile, io
-import onlexnet.pdt.market.events as events
+import market_rpc.onlexnet.pdt.market.events as events
 APP_PORT=os.getenv('APP_PORT', 50052)
 log = logging.getLogger("myapp")
 
-async def serve():
+async def serve(df: DataFrame):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 
     server.add_insecure_port(f"[::]:{APP_PORT}")
     server.start()
 
     with DaprClient() as dc:
-        event = events.MarketChangedEvent(date = 20010203)
         
-        event_as_str = str(event)
-        
-        logging.info("sparta")
         schema_prefix = "onlexnet:v1"
         topic_name = f"{schema_prefix}:{events.MarketChangedEvent.RECORD_SCHEMA.fullname}"
 
-        while True:
-
+        for index, row in df.iterrows():
+            date = row['date']
+            date_as_year = date.year * 10_000 + date.month * 100 + date.day
+            event = events.MarketChangedEvent(date = date_as_year)
+            event_as_str = str(event)
             event_as_dict = event.to_avro_writable()
             as_json = json.dumps(event_as_dict)
             fastavro.json_writer
             resp = dc.publish_event(pubsub_name="pubsub", topic_name=topic_name, data = as_json, data_content_type="application/json")
             logging.info(f"Event sent: {event_as_str}")
-            await asyncio.sleep(3)
+            await asyncio.sleep(0.01)
 
     server.wait_for_termination()
 
@@ -48,13 +52,17 @@ def signal_handler(sig, frame):
     sys.exit(0)
 
 if __name__ == '__main__':
+
+    ctx = yf.LoadContext(date(2020, 1, 1), date(2020, 12, 31), "msft")
+    data = yf.load(ctx)
+
     logging.basicConfig(level=logging.DEBUG)
 
     logger.info(f"port: {APP_PORT}")
     
     loop = asyncio.get_event_loop()
     tasks = [
-        loop.create_task(serve()),
+        loop.create_task(serve(data)),
     ]
     loop.run_until_complete(asyncio.wait(tasks))
     loop.close()
