38a2bbac3592b02bb2f2eeb458aa6be9977fee64
siudeks <slawomir@siudek.net>
2025-03-24 14:29:11 +0000

    Externaly available schema-registry

--- infra/schema-registry.tf
+++ infra/schema-registry.tf
@@ -1,7 +1,5 @@
 resource "kubernetes_service" "schema_registry" {
   metadata {
-    # please do not name your service as 'schema-registry'
-    # https://managing.blue/2019/10/18/port-is-deprecated-please-use-schema_registry_listeners-instead/
     name      = "schema-registry-for-avro"
     namespace = "default"
   }
@@ -11,10 +9,13 @@ resource "kubernetes_service" "schema_registry" {
       app = "schema-registry"
     }
 
+    type = "NodePort"
+
     port {
       protocol    = "TCP"
       port        = 8081
       target_port = 8081
+      node_port   = 30081 # Możesz zmienić na dowolny dostępny port
     }
   }
 }
@@ -61,7 +62,6 @@ resource "kubernetes_deployment" "schema_registry" {
 
           env {
             name  = "SCHEMA_REGISTRY_KAFKASTORE_BOOTSTRAP_SERVERS"
-            # value = "PLAINTEXT://kafka.default.svc.cluster.local:9092"
             value = "PLAINTEXT://kafka-controller-headless:9092"
           }
 
@@ -74,20 +74,3 @@ resource "kubernetes_deployment" "schema_registry" {
     }
   }
 }
-
-# resource "helm_release" "schema_registry" {
-#   name       = "schema-registry"
-#   repository = "https://packages.confluent.io/helm"
-#   chart      = "cp-schema-registry"
-#   version    = "latest"
-
-#   set {
-#     name  = "kafkastore.bootstrap.servers"
-#     value = "PLAINTEXT://kafka:9092"
-#   }
-
-#   set {
-#     name  = "schemaRegistry.listener"
-#     value = "http://0.0.0.0:8081"
-#   }
-# }
