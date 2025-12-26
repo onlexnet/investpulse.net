40d3b6c82bbd156a1a4f98721e04ad0a9bd4eee5
siudeks <slawomir@siudek.net>
2025-12-26 09:41:30 +0000

    Add JaCoCo

---
+++ uservices/twitter/src/main/java/net/investpulse/x/Program.java
@@ -0,0 +1,12 @@
+package net.investpulse.x;
+
+import org.springframework.boot.SpringApplication;
+
+
+public class Program {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RootConfigurer.class, args);
+	}
+
+}
---
+++ uservices/twitter/src/main/java/net/investpulse/x/RootConfigurer.java
@@ -0,0 +1,8 @@
+package net.investpulse.x;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class RootConfigurer {
+    
+}
